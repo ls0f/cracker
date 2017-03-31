ef4a01890b4492295a7c54c1eb6c587e8bd5641a
haifangwang(王海芳) <admin@lovedboy.com>
2017-03-31 14:54:52 +0800

    add http support

--- local/main.go
+++ local/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"http_connect"
 	"logger"
 	"os"
 	"proxy"
 	"socks"
+	"sync"
 )
 
 var (
@@ -17,7 +19,8 @@ var (
 var g = logger.GetLogger()
 
 func main() {
-	laddr := flag.String("laddr", "", "listen addr")
+	socks5Addr := flag.String("socks5", "127.0.0.1:1080", "socks5 listen addr")
+	httpAddr := flag.String("http", "127.0.0.1:8080", "http listen addr")
 	raddr := flag.String("raddr", "", "remote http url(e.g, https://example.com)")
 	secret := flag.String("secret", "", "secret key")
 	debug := flag.Bool("debug", false, "debug mode")
@@ -31,9 +34,28 @@ func main() {
 	}
 	logger.InitLogger(*debug)
 	proxy.Init()
-	s, err := socks.NewSocks5(*laddr, *raddr, *secret)
-	if err != nil {
-		g.Fatal(err)
+	wg := &sync.WaitGroup{}
+	if *socks5Addr != "" {
+		s, err := socks.NewSocks5(*socks5Addr, *raddr, *secret)
+		if err != nil {
+			g.Fatal(err)
+		}
+		wg.Add(1)
+		go func() {
+			s.Wait()
+			wg.Done()
+		}()
 	}
-	s.Wait()
+	if *httpAddr != "" {
+		h, err := http_connect.NewHttpConnect(*httpAddr, *raddr, *secret)
+		if err != nil {
+			g.Fatal(err)
+		}
+		wg.Add(1)
+		go func() {
+			h.Wait()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
